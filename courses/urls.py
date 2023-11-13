0aca732d1aeaa0a3cabc43ffbc665b98bb5dc710
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-11-13 23:16:16 +0300

    add tastypie for api endpoints

--- courses/urls.py
+++ courses/urls.py
@@ -17,7 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from api.models import CourseResource, CategoryResource
+from tastypie.api import  Api
+api = Api(api_name='V1')
+course_resource = CourseResource()
+category_resource = CategoryResource()
+
+api.register(course_resource)
+api.register(category_resource)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls'))
+    path('shop/', include('shop.urls')),
+
 ]
